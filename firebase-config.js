5dec5a9d89ca02285247e75eeec5a7bd8f7d168f
julianmgvass <julianmgvass@gmail.com>
2026-08-22 14:29:01 +0530

    Finalize admin dashboard and dynamic product system

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBw8bcl2SXHuE6VH0U3doGsoXbqIwaSi70",
+  authDomain: "st-joseph-products.firebaseapp.com",
+  projectId: "st-joseph-products",
+  storageBucket: "st-joseph-products.firebasestorage.app",
+  messagingSenderId: "826573831144",
+  appId: "1:826573831144:web:6fe84d91720a5a795c825d",
+  measurementId: "G-6T9L0RWNK6"
+};
